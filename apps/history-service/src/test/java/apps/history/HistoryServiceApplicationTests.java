027a3461e152a2d8698bb73565d3448388ccc987
viniciusnorberto <vinicius.norberto@hubbrasil.com.br>
2025-04-12 14:36:21 -0300

    feat: add microservices structure with appointment, auth, history, notification and user services

---
+++ apps/history-service/src/test/java/apps/history/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package apps.history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
